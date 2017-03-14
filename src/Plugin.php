5a71a38219f551439cb8957bee68dce27e85092f
vkrasnoperov <vkrasnoperov@dealersocket.com>
2017-03-14 07:47:55 +0200

    tests.py moved into tests/ Better package check

--- src/Plugin.php
+++ src/Plugin.php
@@ -11,7 +11,9 @@ class Plugin implements PluginInterface
 {
 	public function activate(Composer $composer, IOInterface $io)
 	{
-		if (in_array('--no-dev', $_SERVER['argv'], true)) {
+		$package = $composer->getPackage();
+
+		if ($package instanceof CompletePackage && in_array('--no-dev', $_SERVER['argv'], true)) {
 			return;
 		}
 
@@ -24,18 +26,14 @@ See README.md for more details.");
 			$devScripts = array_merge_recursive($devScripts, $config['scripts-dev']);
 		}
 
-		$package = $composer->getPackage();
-
 		$extra = $package->getExtra();
 		if (isset($extra['scripts-dev']) && is_array($extra['scripts-dev'])) {
 			$devScripts = array_merge_recursive($devScripts, $extra['scripts-dev']);
 		}
 
-		if ($package instanceof CompletePackage) {
-			foreach ($devScripts as $event => &$listeners) {
-				$listeners = (array) $listeners;
-			}
-			$package->setScripts(array_merge_recursive($package->getScripts(), $devScripts));
+		foreach ($devScripts as $event => &$listeners) {
+			$listeners = (array)$listeners;
 		}
+		$package->setScripts(array_merge_recursive($package->getScripts(), $devScripts));
 	}
 }
