35581b680d5a8ff8309c4008d07897e36a2c857d
Krasnoperov Vitaliy <aliatar.neron@gmail.com>
2017-12-20 10:43:09 +0200

    Add test with custom script, refer #4

--- tests/tests.py
+++ tests/tests.py
@@ -2,11 +2,12 @@ from subprocess import check_output, check_call
 import os
 
 
-def test(test, args=[]):
+def test(test, args=[], cmd=['composer', 'update', '-v'], clean_dir=True):
     working_dir = '/tmp/scriptsdev/' + test
 
-    check_call(['rm', '-rf', working_dir])
-    check_call(['mkdir', '-p', working_dir])
+    if clean_dir:
+        check_call(['rm', '-rf', working_dir])
+        check_call(['mkdir', '-p', working_dir])
 
     source_composer_json = open('tests/%s.json' % test, 'r').read()
     source_composer_json = source_composer_json.replace('<PLUGIN_PATH>', os.getcwd())
@@ -15,22 +16,33 @@ def test(test, args=[]):
     target_composer_json.write(source_composer_json)
     target_composer_json.close()
 
-    return check_output(['composer', 'update', '-v'] + args, cwd=working_dir)
+    return check_output(cmd + args, cwd=working_dir)
 
 
 def check(expect, actual):
+    print "expect: ", expect
+    print "actual: ", actual
     if not expect in actual:
         raise Exception('EXPECTED\n"%s"\nBUT FOUND\n"%s"' % (expect, actual))
 
 
-def check_not(expect, actual):
-    if expect in actual:
-        raise Exception('EXPECTED\n"%s"\nBUT FOUND\n"%s"' % (expect, actual))
+def check_not(expect_not, actual):
+    print "expect not: ", expect_not
+    print "actual: ", actual
+    if expect_not in actual:
+        raise Exception('EXPECTED\n"%s"\nBUT FOUND\n"%s"' % (expect_not, actual))
 
 
 ############ TESTS HERE #############
 
 check('SCRIPTSDEV RULEZ', test('extra'))
-check('SCRIPTSDEV RULEZ', test('legacy'))
 check_not('SCRIPTSDEV RULEZ', test('extra', ['--no-dev']))
+
+check('SCRIPTSDEV RULEZ', test('legacy'))
 check_not('SCRIPTSDEV RULEZ', test('legacy', ['--no-dev']))
+
+check('SCRIPTSDEV RULEZ', test('extra-with-custom-script', cmd=['composer', 'run-script', 'test-update']))
+check_not('SCRIPTSDEV RULEZ', test('extra-with-custom-script', cmd=['composer', 'run-script', 'test-update-no-dev']))
+
+check('SCRIPTSDEV RULEZ', test('extra-with-custom-script', cmd=['composer', 'run-script', 'test-install'], clean_dir=False))
+check_not('SCRIPTSDEV RULEZ', test('extra-with-custom-script', cmd=['composer', 'run-script', 'test-install-no-dev'], clean_dir=False))
