79ff8318273321c828562c7eb47c794bb47d32ee
Fabien Villepinte <fabien.villepinte@gmail.com>
2020-07-11 14:14:23 +0200

    Add support for Composer 2

--- src/Plugin.php
+++ src/Plugin.php
@@ -25,4 +25,12 @@ class Plugin implements PluginInterface, Capable
 
         $package->setScripts(array_merge_recursive($package->getScripts(), $devScripts));
     }
+
+    public function deactivate(Composer $composer, IOInterface $io)
+    {
+    }
+
+    public function uninstall(Composer $composer, IOInterface $io)
+    {
+    }
 }
